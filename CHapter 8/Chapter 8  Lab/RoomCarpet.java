eb33ca76f230c31b3dffa37a4110ba5cf6631477
Oinklet001 <trwer1422@gmail.com>
2017-04-25 18:38:13 -0700

    Create RoomCarpet.java

---
+++ CHapter 8/Chapter 8  Lab/RoomCarpet.java
@@ -0,0 +1,16 @@
+public class RoomCarpet
+{
+private double cost;
+ 
+ public RoomCarpet(double cost){
+ 
+RoomDemension RoomDem = new RoomDemension();
+double size = RoomDem.getArea();
+ 
+ 
+ }
+
+
+
+
+}
