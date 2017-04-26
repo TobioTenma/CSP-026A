4ea7e5a3c455159c4528349429230a5186df328c
Alisson Imelda Leiva Salazar <tobiotenma@hotmail.com>
2017-04-25 21:29:58 -0700

    NEW FIXES FOR Oinklet001
    From TobioTenma
    Cheers!

---
+++ CHapter 8/Chapter 8  Lab/RoomDimension.java
@@ -0,0 +1,53 @@
+
+
+public class RoomDimension
+{
+ //Initilazation
+ private static double width;
+ private static double length;
+ 
+ //constructors
+ public RoomDimension(double width, double length){
+ 
+ this.width = width;
+ this.length = length;
+ 
+ 
+ }
+ 
+ public RoomDimension(RoomDimension rd){
+ 
+ this.width = rd.getwidth();
+ 
+ this.length = rd.getlength();
+ 
+ }
+ 
+ //Methods
+ public double getwidth(){
+ 
+  return width;
+ 
+ }
+ 
+ public double getlength(){
+ 
+   return length;
+ }
+ 
+   //Get the area
+ public static double getArea(){
+   double area = length * width;
+   
+   return area;
+   
+   }
+   
+  public String toString() {
+	return "";
+}
+  
+
+  
+
+}
