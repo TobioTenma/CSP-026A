644a1065f4349f620b0bc0e5d6ba9b943fcb8741
Oinklet001 <trwer1422@gmail.com>
2017-05-07 12:50:18 -0700

    Create SavingsAccount.java

---
+++ Chapter 10/Chapter 11 Practice/SavingsAccount.java
@@ -0,0 +1,49 @@
+public class SavingAccountTest extends BankAccountTest
+{
+  private int deposite;
+  
+  private int withdrawls;
+  
+  private int minAmount = 25;
+  
+  private int total;
+  
+  private int years; 
+  
+  
+@Override
+  
+  public boolean desposit (double total)
+   {
+       if (minAmount > total) 
+       {
+           return true;
+        }
+        else return false;
+    }
+@Override
+    public boolean withdraw (double total)
+   {
+       if (true) 
+       {
+          super.withdraw(total);
+           return true;
+        }
+        else return false;
+    }
+
+   public abstract void bankingFees() (
+
+   if withdraw > 4
+   (
+      total = years * 10 + 1;
+      
+   )
+   
+   else 
+   
+   return false   
+) 
+
+
+)
