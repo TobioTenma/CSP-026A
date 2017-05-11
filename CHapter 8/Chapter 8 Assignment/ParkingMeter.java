fe264935eb1c4318fc37b7ac97d7be0891e59d2e
Oinklet001 <trwer1422@gmail.com>
2017-05-10 21:09:53 -0700

    Create ParkingMeter.java

---
+++ CHapter 8/Chapter 8 Assignment/ParkingMeter.java
@@ -0,0 +1,19 @@
+class ParkingMeter {
+
+    private int boughtTime;
+
+    public ParkingMeter(int boughtTime){
+    
+    boughtTime = boughtTime;
+    
+    }
+   
+    public void createboughtTime() 
+       {
+       
+       this.boughtTime = boughtTime;
+       
+       }
+
+
+}
