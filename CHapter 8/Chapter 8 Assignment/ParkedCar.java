e06c5bbd871e4041f7b6d96635804537c1f63eda
Oinklet001 <trwer1422@gmail.com>
2017-05-11 21:50:54 -0700

    Update ParkedCar.java

--- CHapter 8/Chapter 8 Assignment/ParkedCar.java
+++ CHapter 8/Chapter 8 Assignment/ParkedCar.java
@@ -1,56 +1,57 @@
-class ParkedCar {
+public class ParkedCar {
+	 private String make;
+	    private String model;
+	    private String color;
+	    private double licenseNum;
+	    private double hour;
 
-    private String make;
-    private String model;
-    private String color;
-    private int licenseNum;
-    private int minutes;
-    
-    public ParkedCar(String make, String model, String color, int licenseNum, int minutes){
-    
-    make = make;
-    model = model;
-    color = color;
-    licenseNum = licenseNum;
-    minutes = minutes;
-    
-    }
-    
-    
-       public void createMake() 
-       {
-       
-       this.make = make;
-       
-       }
 
-       public void createModel() 
-       {
-       
-       this.model = model;
-       
-       }
-       
-       public void createColor() 
-       {
-       
-       this.color = color;
-       
-       }
-       
-       
-       public void createLicenseNum() 
-       {
-       
-       this.licenseNum = licenseNum;
-       
-       }
-       
-       public void createMinutes() 
-       {
-       
-       this.minutes = minutes;
-       
-       }
+	    //constructor
+	    public ParkedCar(String make, String model, String color, double licenseNum, double hour){
 
-    }
+	    make = make;
+	    model = model;
+	    color = color;
+	    licenseNum = licenseNum;
+	    hour = hour;
+
+	    }
+
+
+	       public void setMake(String make)
+	       {
+
+	       this.make = make;
+
+	       }
+
+	       public void setModel(String model)
+	       {
+
+	       this.model = model;
+
+	       }
+
+	       public void setColor(String color)
+	       {
+
+	       this.color = color;
+
+	       }
+
+
+	       public void setLicenseNum(double licenseNum)
+	       {
+
+	       this.licenseNum = licenseNum;
+
+	       }
+
+	       public void setHour(int hour)
+	       {
+
+	       this.hour = hour;
+
+	       }
+
+}
